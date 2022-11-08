b92441b440f272ee39e5960ced13255d290a834c
chanchal26 <chanchal.mch2613@gmail.com>
2022-11-08 13:29:40 +0600

    Folder structure done

---
+++ src/Context/UserContext.jsx
@@ -0,0 +1,97 @@
+import {
+    createUserWithEmailAndPassword,
+    getAuth,
+    GithubAuthProvider,
+    GoogleAuthProvider,
+    onAuthStateChanged,
+    sendEmailVerification,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    signOut,
+    updateProfile,
+} from 'firebase/auth'
+import React, { createContext, useEffect, useState } from 'react'
+import Swal from 'sweetalert2';
+import app from '../Config/Firebase.config';
+
+
+const auth = getAuth(app);
+export const AuthContext = createContext()
+
+const UserContext = ({ children }) => {
+
+    const googleProvider = new GoogleAuthProvider()
+    const githubProvider = new GithubAuthProvider()
+
+    const [user, setUser] = useState({})
+    const [loading, setLoading] = useState(true)
+
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+
+
+    const updateName = (name, photoURL) => {
+        setLoading(true)
+        return updateProfile(auth.currentUser, { displayName: name, photoURL: photoURL, })
+    }
+
+
+
+    const signInWithGoogle = () => {
+        setLoading(true)
+        return signInWithPopup(auth, googleProvider)
+    }
+
+    const signInWithGithub = () => {
+        setLoading(true)
+        return signInWithPopup(auth, githubProvider)
+    }
+
+
+    const logout = () => {
+        Swal.fire(
+            'You have successfully signed Out!'
+        )
+        setLoading(true)
+        return signOut(auth)
+    }
+
+
+    const login = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    // useEffect(() => {
+
+    //     const unsubscribe = onAuthStateChanged(auth, currentUser => {
+    //         setUser(currentUser)
+    //         setLoading(false)
+    //     })
+
+    //     return () => {
+
+    //         unsubscribe()
+    //     }
+    // }, [])
+
+    // const authInfo = {
+    //     user,
+    //     createUser,
+    //     updateName,
+    //     signInWithGoogle,
+    //     signInWithGithub,
+    //     logout,
+    //     login,
+    //     loading,
+    // }
+
+    // return (
+    //     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+    // );
+};
+
+export default UserContext;
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,44 @@
+import { createBrowserRouter } from "react-router-dom";
+import Blog from "../Components/Blog";
+import ErrorPage from "../Components/ErrorPage";
+import Home from "../Components/Home/Home";
+import Login from "../Components/Login";
+import Register from "../Components/Register";
+import Main from "../Layout/Main";
+import PrivateRoutes from "./PrivateRoutes";
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main />,
+        children: [
+            // {
+            //     path: '/',
+            //     element: <Home />
+            // },
+            // {
+            //     path: '/home',
+            //     element: <Home />
+            // },
+            // {
+            //     path: '/blog',
+            //     element: <PrivateRoutes><Blog /></PrivateRoutes>
+            // },
+            // {
+            //     path: '/login',
+            //     element: <Login />
+            // },
+            // {
+            //     path: '/register',
+            //     element: <Register />
+            // },
+        ]
+    },
+    {
+        path: '*',
+        element: <ErrorPage />
+    }
+])
+
+export default router;
